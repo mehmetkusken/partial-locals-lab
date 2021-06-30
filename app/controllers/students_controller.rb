40b9db8bd131a6e41b3c7940057e30b3251c0e00
Mehmet <mehmetkusken@gmail.com>
2021-06-30 14:18:54 -0600

    commit tree 0cbac012d2c29495d2c94bf54aa4d8e24fcd5fd4

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,6 @@ class StudentsController < ApplicationController
   def new
     @student = Student.new
   end
-
   def create
     @student = Student.new(student_params)
     if @student.save
@@ -11,18 +10,21 @@ class StudentsController < ApplicationController
       render 'new'
     end
   end
-
   def edit
     @student = Student.find(params[:id])
   end
-
   def show
     @student = Student.find(params[:id])
   end
 
   def index
     @students = Student.all
-  end
+    if params[:student][:name]
+      @students = Student.search(params[:student][:name])
+       else
+        @students = Student.all
+      end
+    end
 
   def student_params
     params.require(:student).permit(:name, :birthday, :hometown)
